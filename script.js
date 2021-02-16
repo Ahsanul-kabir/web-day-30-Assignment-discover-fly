a9507e2e6071d89373b3cca7052961d1c9474c4f
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-02-16 20:28:08 +0600

    updated

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // handle First Class Ticket
+var firstClassTicketTotal;
 incBtnForFirstClass = document.getElementById('first-classs-ticket-increase-btn');
 incBtnForFirstClass.addEventListener('click', function() {
     const firstClassInput = document.getElementById('first-classs-ticket-text-field');
     const inputCount = parseInt(firstClassInput.value);
+
     const inputNewCount = inputCount + 1;
+
     firstClassInput.value = inputNewCount;
     firstClassTicketTotal = inputNewCount * 150;
     document.getElementById('firstClassTicketTotal-id').innerText = firstClassTicketTotal;
@@ -16,19 +19,29 @@ decBtnForFirstClass = document.getElementById('first-classs-ticket-decrease-btn'
 decBtnForFirstClass.addEventListener('click', function() {
     const firstClassInput = document.getElementById('first-classs-ticket-text-field');
     const inputCount = parseInt(firstClassInput.value);
+
     const inputNewCount = inputCount - 1;
+
     firstClassInput.value = inputNewCount;
     firstClassTicketTotal = inputNewCount * 150;
     document.getElementById('firstClassTicketTotal-id').innerText = firstClassTicketTotal;
+
+
+
 });
 
 
+
+
 // handle Economy Class Ticket
+var economyClassTicketTotal;
 incBtnForEconomyClass = document.getElementById('economy-classs-ticket-increase-btn');
 incBtnForEconomyClass.addEventListener('click', function() {
     const economyClassInput = document.getElementById('economy-classs-ticket-text-field');
     const inputCount = parseInt(economyClassInput.value);
+
     const inputNewCount = inputCount + 1;
+
     economyClassInput.value = inputNewCount;
     economyClassTicketTotal = inputNewCount * 100;
     document.getElementById('economyClassTicketTotal-id').innerText = economyClassTicketTotal;
@@ -41,10 +54,18 @@ incBtnForEconomyClass = document.getElementById('economy-classs-ticket-deccrease
 incBtnForEconomyClass.addEventListener('click', function() {
     const economyClassInput = document.getElementById('economy-classs-ticket-text-field');
     const inputCount = parseInt(economyClassInput.value);
+
     const inputNewCount = inputCount - 1;
+
     economyClassInput.value = inputNewCount;
     economyClassTicketTotal = inputNewCount * 100;
     document.getElementById('economyClassTicketTotal-id').innerText = economyClassTicketTotal;
 
 
 });
+
+
+
+const subTotal = Math.round(firstClassTicketTotal + economyClassTicketTotal);
+console.log(subTotal);
+//document.getElementById('sub-total').innerText = subTotal.value;
